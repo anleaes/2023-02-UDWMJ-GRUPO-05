1b2d2fc1d0c7d5dee59e26ed8c69faeffd06644a
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-09-28 16:48:48 -0300

    arrumada a classe

--- product.py
+++ product.py
@@ -1,10 +1,7 @@
-from Category import category
-
 class Product:
-    
-    def __init__(self, name , description , date_fabrication, is_active):
+    def __init__(self, name , description , date_fabrication, is_active, category):
         self.name = name
         self.description = description
         self.date_fabrication = date_fabrication
         self.is_active = is_active
-        
+        self.category = category
