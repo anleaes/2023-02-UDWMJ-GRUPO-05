cae08de7cc802ee002c2c67fcf1414c723fac557
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-09-28 16:32:19 -0300

    arrumada a classe produtos

--- product.py
+++ product.py
@@ -1,6 +1,8 @@
 class Product:
     
-    def __init__(self, date_fabrication, is_active):
+    def __init__(self, name , description , date_fabrication, is_active):
+        self.name = name
+        self.description = description 
         self.date_fabrication = date_fabrication
         self.is_active = is_active
         
