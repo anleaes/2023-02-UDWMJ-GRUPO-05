325ad192ded3eeb10f28488ead0772311123a415
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-09-28 16:35:52 -0300

    classe modificada

--- product.py
+++ product.py
@@ -1,6 +1,6 @@
 class Product:
     
-    def __init__(self, name , description , date_fabrication, is_active, category):
+    def __init__(self, name , description , date_fabrication, is_active):
         self.name = name
         self.description = description
         self.date_fabrication = date_fabrication
