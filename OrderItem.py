19359e9265b1a3c0d068e9fd84f85767b5fe1326
lucasckrug <lucasckrug@hotmail.com>
2023-09-29 17:03:21 -0300

    classe criada

--- OrderItem.py
+++
@@ -1,6 +0,0 @@
-class orderitem:
-    def __init__(self, quantity, unitary_price, order, product):
-        self.quantity = quantity
-        self.unitary_price = unitary_price
-        self.order = order
-        self.product = product
