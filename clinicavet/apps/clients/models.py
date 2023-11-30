d31ec54ea6b779387ee78bafbd9feed9e548fe22
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-11-30 13:25:40 -0300

    criado a classe clientsocialnetwork

--- clinicavet/apps/clients/models.py
+++ clinicavet/apps/clients/models.py
@@ -25,3 +25,18 @@ class Client(models.Model):
 
     def __str__(self):
         return self.first_name
+    
+    class ClientSocialnetwork(models.Model):
+        created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    socialnetwork = models.ForeignKey(Socialnetwork, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Item de Redes Social'
+        verbose_name_plural = 'Itens de Rede Social'
+        ordering =['id']
+
+    def __str__(self):
+        return self.socialnetwork.name
+
