9a4840523aad09c93ebaa83e83d68ecb520ad0ca
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-11-29 23:00:19 -0300

    adicionado o path socialnetworks

--- projetoapp/projetoapp/urls.py
+++ projetoapp/projetoapp/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('categorias/', include('categories.urls', namespace='categories')),
+    path('redessociais/', include('socialnetworks.urls', namespace='socialnetworks')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
