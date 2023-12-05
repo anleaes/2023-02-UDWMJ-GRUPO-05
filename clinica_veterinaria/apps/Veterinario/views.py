9cb651bd2d83b96f21ad052dafdd1359754724d0
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-05 15:54:54 -0300

    Criado e configurado views.py

--- clinica_veterinaria/apps/Veterinario/views.py
+++ clinica_veterinaria/apps/Veterinario/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Veterinario.forms import VeterinarioForm
 
-# Create your views here.
+def cadastrar_Veterinario(request):
+    if request.method == 'POST':
+        form = VeterinarioForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sucesso!')
+        
+        
+    else:
+        form = VeterinarioForm()
+        
+    return render(request, 'cadastrar_veterinario.html', {'forms': form})
