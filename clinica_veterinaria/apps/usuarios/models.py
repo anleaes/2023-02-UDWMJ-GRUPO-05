5dd2c3cf5a791387b5800662c0871c023ebbdeaa
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-12-06 16:16:30 -0300

    criado a classe Usuario

--- clinica_veterinaria/apps/usuarios/models.py
+++ clinica_veterinaria/apps/usuarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.db import models
+
+class Usuario(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
 
