32b12c91151c3c17d547a83ea203a9ba7e71dfe1
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-12-06 15:25:58 -0300

    feito e moostrado os path no urls.py

---
+++ clinica_veterinaria/apps/animals/urls.py
@@ -0,0 +1,10 @@
+from animals import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('create/', views.create_animal, name="create_animal"),
+    path('update/<int:id>', views.update_animal, name="update_animal"),
+    path('delete/<int:id>', views.delete_animal, name="delete_animal"),
+    
+]
