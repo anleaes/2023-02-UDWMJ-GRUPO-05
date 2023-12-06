4ffcf065348fe89ede85fa8cabb5de5782db0e88
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-05 21:49:34 -0300

    Veterinario/ setado nas urls da raiz e no settings

--- clinica_veterinaria/clinica_veterinaria/urls.py
+++ clinica_veterinaria/clinica_veterinaria/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('Veterinario/',include('apps.Veterinario.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
