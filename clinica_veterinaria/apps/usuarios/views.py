3f99f17d86fa32dab4436aaa0a6089fc378f8d32
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-12-06 16:17:19 -0300

    criado o 2 metodos na view

--- clinica_veterinaria/apps/usuarios/views.py
+++ clinica_veterinaria/apps/usuarios/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from usuarios.models import Usuario
+from usuarios.forms import UsuarioForm
 
-# Create your views here.
+
+def index_usuario(request):
+    usuarios = Usuario.objects.all()
+    context = {"usuarios" : usuarios}
+    return render(request, "usuarios/list_usuarios.html", context)
+
+def create_usuario(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        cpf = request.POST.get("cpf")
+        endereco = request.POST.get("endereco")
+        email = request.POST.get("email")
+        telefone = request.POST.get("telefone")
+        
+        usuario = Usuario(name=name, cpf=cpf, endereco=endereco, email=email, telefone=telefone)
+        usuario.save()
+        return redirect("index_usuario")
+    else:
+        form = UsuarioForm()
+        
+    return render(request, "usuarios/create_usuarios.html", {"form" : form})
