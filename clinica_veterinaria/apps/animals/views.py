67e379c859bf707231964507067e7bf3c8027020
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-12-06 15:21:31 -0300

    criado os metodos no view.py

--- clinica_veterinaria/apps/animals/views.py
+++ clinica_veterinaria/apps/animals/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from animals.models import Animal
+from animals.forms import AnimalForm
 
-# Create your views here.
+
+def index(request):
+    animals = Animal.objects.all()
+    context = {"animals" : animals}
+    return render(request, "animals/list_animals.html", context)
+
+def create_animal(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        age = request.POST.get('age')
+        weight = request.POST.get('weight')
+        animal = Animal(name=name, age=age, weight=weight)
+        animal.save()
+        return redirect('index')
+    else:
+        form = AnimalForm()
+    return render(request, 'animals/create_animal.html', {'form': form})
