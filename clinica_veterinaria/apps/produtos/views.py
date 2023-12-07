2aa266fbbb1065485125fd7776d24fef921541b3
lucasckrug <lucasckrug@hotmail.com>
2023-12-06 21:47:22 -0300

    create_produto inserido na views

--- clinica_veterinaria/apps/produtos/views.py
+++ clinica_veterinaria/apps/produtos/views.py
@@ -6,3 +6,18 @@ def index_produto(request):
     produtos = Produto.objects.all()
     context = {"produtos" : produtos}
     return render(request, "produtos/list_produtos.html", context)
+
+def create_produto(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        descricao = request.POST.get("descricao")
+        quantidade = request.POST.get("quantidade")
+        preco = request.POST.get("preco")
+
+        produto = Produto(nome=nome, descricao=descricao, quantidade=quantidade, preco=preco)
+        produto.save()
+        return redirect("index_produto")
+    else:
+        form = ProdutoForm
+        
+    return render(request, "produtos/create_produtos.html", {"form" : form})
