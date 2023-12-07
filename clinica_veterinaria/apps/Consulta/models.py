9965a8ddd6fbc3909e956def68967fe431ad2066
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 14:30:38 -0300

    Classe consulta criada e atributos definidos em models.py

--- clinica_veterinaria/apps/Consulta/models.py
+++ clinica_veterinaria/apps/Consulta/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from Veterinario.models import Veterinario
+from animals.models import Animal
 
-# Create your models here.
+
+class Consulta(models.Model):
+    data = models.DateField()
+    hora = models.TimeField()
+    observacao = models.TextField()
+    veterinario = models.ForeignKey(Veterinario, on_delete=models.CASCADE)
+    animal_nome = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Consulta marcada para: {self.animal.nome} no dia {self.data} às {self.hora} com o(a) Dr(a) {self.veterinario.nome}"
+    
+    
