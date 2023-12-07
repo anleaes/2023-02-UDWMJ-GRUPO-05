041bb2b11485ce51ff7a60de159ca2f8add877b7
lucasckrug <lucasckrug@hotmail.com>
2023-12-06 21:40:29 -0300

    models de produto criado

--- clinica_veterinaria/apps/produtos/models.py
+++ clinica_veterinaria/apps/produtos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+    quantidade = models.CharField(max_length=100)
+    preco = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
