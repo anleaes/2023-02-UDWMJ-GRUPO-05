813d537f0e62a5e3a25da20571eb7324e89c438d
lucasckrug <lucasckrug@hotmail.com>
2023-12-07 10:54:31 -0300

    create servico criado na views

--- clinica_veterinaria/apps/servicos/views.py
+++ clinica_veterinaria/apps/servicos/views.py
@@ -6,3 +6,19 @@ def index_servico(request):
     servicos = Servicos.objects.all()
     context = {"servicos" : servicos}
     return render(request, "servicos/list_servicos.html", context)
+
+def create_servico(request):
+    if request.method == "POST":
+        banho = request.POST.get("banho")
+        tosa = request.POST.get("tosa")
+        nome = request.POST.get("nome")
+        animal = request.POST.get("animal")
+        raca = request.POST.get("raca")
+
+        servico = Servicos(banho=banho, tosa=tosa, nome=nome, animal=animal, raca=raca)
+        servico.save()
+        return redirect("index_servico")
+    else:
+        form = ServicosForm
+        
+    return render(request, "servicos/create_servicos.html", {"form" : form})
