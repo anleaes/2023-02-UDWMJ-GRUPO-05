ec05bea0cebd7ff0d3a5a7cfb511be29a397f821
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 10:15:06 -0300

    Views.py configurada

--- clinica_veterinaria/apps/Veterinario/views.py
+++ clinica_veterinaria/apps/Veterinario/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from Veterinario.forms import VeterinarioForm
+from Veterinario.models import Veterinario
 
-# Create your views here.
+def listar_Veterinarios(request):
+    veterinarios = Veterinario.objects.all()
+    return render(request, 'Veterinario/listar_veterinarios.html', {'veterinarios': veterinarios})
