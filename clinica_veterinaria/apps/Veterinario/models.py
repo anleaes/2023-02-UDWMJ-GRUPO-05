df5f751235f66fe1d95298aed6c87dc7cf3336ad
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 10:07:05 -0300

    Classe Veterinario criada

--- clinica_veterinaria/apps/Veterinario/models.py
+++ clinica_veterinaria/apps/Veterinario/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Veterinario(models.Model):
+    AVES = 'Aves'
+    GATO = 'Gato'
+    CÃES = 'Cães'
+    OUTRO = 'Outro'
+    
+    ESPECIALIZACAO_ESCOLHA = [
+        (AVES, 'Aves'),
+        (GATO, 'Gato'),
+        (CÃES, 'Cães'),
+        (OUTRO, 'Outro'),
+    ]
+    
+    nome = models.CharField(max_length=255)
+    especializacao = models.CharField(max_length=255, choices=ESPECIALIZACAO_ESCOLHA)
+    
+    def __str__(self):
+        return self.nome
