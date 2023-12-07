896053825bf8108579c02e4a4a60b56158282a78
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 16:16:58 -0300

    urls ajustadas por conta de erro

--- clinica_veterinaria/apps/Consulta/urls.py
+++ clinica_veterinaria/apps/Consulta/urls.py
@@ -3,7 +3,7 @@ from Consulta import views
 
 urlpatterns = [
     path('listar_consultas', views.listar_consultas, name='listar_consultas'),
-    path('cadastrar/', views.nova_consulta, name='listar_consultas'),
+    path('cadastrar/', views.nova_consulta, name='nova_consulta'),
     path('excluir/<int:id>', views.excluir_consulta, name='excluir_consulta'),
 ]
 
