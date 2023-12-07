f3e7038bc7a38c61658dc40e704d501e4e313997
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 14:43:03 -0300

    Método nova_consulta adicionado em views

--- clinica_veterinaria/apps/Consulta/views.py
+++ clinica_veterinaria/apps/Consulta/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from Consulta.forms import ConsultaForm
+from Consulta.models import Consulta
 
-# Create your views here.
+
+
+
+def nova_consulta(request):
+    if request.method == 'POST':
+        form = ConsultaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_consultas.html')  #  aqui deve ser o redirect para listar consultas
+    else:
+        form = ConsultaForm()
+
+    return render(request, 'nova_consulta.html', {'form': form}) # aqui deve ser o render do agendamento
+                                                                 # talvez seja necessario adicionar 
+                                                                 # 'Consulta/nova_consulta.html'
