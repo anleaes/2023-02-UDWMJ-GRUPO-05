c8f591fcabd414dd056952c1e7770ddc9c6f5f53
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 10:15:56 -0300

    Métodos adicionados em views.py de Veterinario

--- clinica_veterinaria/apps/Veterinario/views.py
+++ clinica_veterinaria/apps/Veterinario/views.py
@@ -5,3 +5,20 @@ from Veterinario.models import Veterinario
 def listar_Veterinarios(request):
     veterinarios = Veterinario.objects.all()
     return render(request, 'Veterinario/listar_veterinarios.html', {'veterinarios': veterinarios})
+
+def cadastrar_Veterinario(request):
+    if request.method == 'POST':
+        form = VeterinarioForm(request.POST)   # talvez seja necessario alterar por conta do nome 
+        if form.is_valid():
+            form.save()
+            return redirect("listar_veterinarios") # redireciona para o index de veterinarios              
+    else:
+        form = VeterinarioForm()
+        
+    return render(request, 'Veterinario/cadastrar_veterinario.html', {'form': form})
+
+
+def excluir_veterinario(request, id):
+    veterinario = get_object_or_404(Veterinario, pk=id)
+    veterinario.delete()
+    return redirect('listar_veterinarios')
