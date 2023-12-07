4619c90bb555a032f527b31db3f315a335d379a8
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 16:16:16 -0300

    Linha alterada em models

--- clinica_veterinaria/apps/Consulta/models.py
+++ clinica_veterinaria/apps/Consulta/models.py
@@ -11,6 +11,6 @@ class Consulta(models.Model):
     animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"Consulta marcada para: {self.animal.nome} no dia {self.data} às {self.hora} com o(a) Dr(a) {self.veterinario.nome}"
+        return self.nome
     
     
