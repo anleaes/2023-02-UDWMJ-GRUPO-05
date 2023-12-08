46099068e1f2ea7dbbf178c6afafec43266d48c3
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 21:28:15 -0300

    excluir uma adocao adicionada em urls.py

--- clinica_veterinaria/apps/Adocao/urls.py
+++ clinica_veterinaria/apps/Adocao/urls.py
@@ -4,4 +4,5 @@ from Adocao import views
 urlpatterns = [
     path('listar_adocoes', views.listar_adocoes, name='listar_adocoes'),
     path('cadastrar/', views.nova_entrada, name='nova_entrada'),
+    path('excluir/<int:id>', views.excluir_adocao, name='excluir_adocao'),
 ]
