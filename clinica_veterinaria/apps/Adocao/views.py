e676c53b204bcfc916d48d725cb2507a6c29014b
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 21:18:39 -0300

    metodo nova entrada adicionado em views

--- clinica_veterinaria/apps/Adocao/views.py
+++ clinica_veterinaria/apps/Adocao/views.py
@@ -7,5 +7,19 @@ def listar_adocoes(request):
     adocoes = Adocao.objects.all()
     return render(request, 'Adocao/listar_adocoes.html', {'adocoes': adocoes})
 
+def nova_entrada(request):
+    if request.method == 'POST':
+        form = AdocaoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_adocoes')  #  aqui deve ser o redirect para listar consultas
+    else:
+        form = AdocaoForm()
+
+    return render(request, 'Adocao/nova_entrada.html', {'form': form}) 
+# aqui deve ser o render do agendamento
+# talvez seja necessario adicionar 
+# 'Consulta/nova_consulta.html'
+
 
 
