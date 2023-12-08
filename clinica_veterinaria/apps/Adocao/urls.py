b50fc7a59edf2be294b2a715ac6e2fb8f31e1102
pedrofleckk <pedrofleckrex@gmail.com>
2023-12-07 21:27:27 -0300

    url para nova entrada adicionada em urls.py

--- clinica_veterinaria/apps/Adocao/urls.py
+++ clinica_veterinaria/apps/Adocao/urls.py
@@ -3,5 +3,5 @@ from Adocao import views
 
 urlpatterns = [
     path('listar_adocoes', views.listar_adocoes, name='listar_adocoes'),
-    
+    path('cadastrar/', views.nova_entrada, name='nova_entrada'),
 ]
